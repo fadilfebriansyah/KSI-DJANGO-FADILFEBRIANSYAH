0d52efe3023ce0d0138155bc0d4ebc00f38cf05b
fadilfebriansyah <fadilfebriansyah76@gmail.com>
2022-12-14 09:26:16 +0700

    django form

--- fadil_django/blog/views.py
+++ fadil_django/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
     db = Post.objects.all()
@@ -10,3 +11,21 @@ def index(request):
         'post':db,
     }
     return render (request, 'blog/index.html',context)
+
+def recent(request):
+    return HttpResponse("ini blog")
+
+
+def form(request):
+    context = {
+        'nama':'nama',
+        'alamat':'alamat',
+        'post':form,
+    }
+    if request.method == 'POST':
+        print("ini adalah method post")
+        context['nama'] = request.POST['nama']
+        context['alamat'] = request.POST['alamat']
+    else:
+        print("ini adalah method get")
+    return render (request, 'blog/form.html',context)
--- fadil_django/fadil_django/views.py
+++ fadil_django/fadil_django/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return render (request,'index.html')
+def articles(request, year):
+    year = year
+    str = year
+    return HttpResponse(year)
 
+
+def about(request):
+    return HttpResponse("ini about")
+
+def index(request):
+    context = {
+        'heading':'Form Manual',
+    }
+    if request.method == 'POST':
+        print("ini adalah method post")
+        context['nickname'] = request.POST['nickname']
+        context['alamat'] = request.POST['alamat']
+    else:
+        print("ini adalah method GET")
+    return render(request, 'index.html', context)
